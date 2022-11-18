9387b010c4251fb0d9c7d3fdb943dffc1b7b8d1a
Aliyan Malik <aliyan.malik.am@gmail.com>
2022-11-19 04:51:39 +0500

    Added resizing functionality

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
 const isDev = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
@@ -87,6 +89,45 @@ const menu = [
   //   ],
   // }
 ];
+
+// Respond to the resize image event
+ipcMain.on("image:resize", (e, options) => {
+  // console.log(options);
+  options.dest = path.join(os.homedir(), "imageresizer");
+  resizeImage(options);
+});
+
+// Resize and save image
+async function resizeImage({ imgPath, height, width, dest }) {
+  try {
+    // console.log(imgPath, height, width, dest);
+
+    // Resize image
+    const newPath = await resizeImg(fs.readFileSync(imgPath), {
+      width: +width,
+      height: +height,
+    });
+
+    // Get filename
+    const filename = path.basename(imgPath);
+
+    // Create destination folder if it doesn't exist
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
+    }
+
+    // Write the file to the destination folder
+    fs.writeFileSync(path.join(dest, filename), newPath);
+
+    // Send success to renderer
+    mainWindow.webContents.send("image:done");
+
+    // Open the folder in the file explorer
+    shell.openPath(dest);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 // QUIT APPLICATION IF ALL WINDOWS ARE CLOSED ON WINDOWS
 app.on("window-all-closed", () => {
