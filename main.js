accec56d88b9af0804d1e8454c4669ea73ce777b
Aliyan Malik <aliyan.malik.am@gmail.com>
2022-11-19 02:12:59 +0500

    Rendered the first window

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("HELLO WORLD");
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+// CREATE MAIN WINDOW HANDLER
+const createMainWindow = () => {
+  const mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: 500,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+};
+
+// RENDER MAIN WINDOW WHEN APP IS LOADED
+app.whenReady().then(() => {
+  createMainWindow();
+});
